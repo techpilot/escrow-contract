2ae8b78c68553fe2c65da8697c63c23276d9f6e0
Stephen Ngwu <ngwustephen99@gmail.com>
2022-06-15 21:03:29 +0100

    added non-reentrant security check

--- client/lib/constants.js
+++ client/lib/constants.js
@@ -1,4 +1,5 @@
-import abi from "../../smart_contract/artifacts/contracts/Transactions.sol/Transactions.json";
+import abi from "./Transactions.json";
+// ../../smart_contract/artifacts/contracts/Transactions.sol/Transactions.json
 
 export const contractAddress = "0x27e08a9F7eDE0683dA7Bd7965084ba26e41Acc49";
 export const contractABI = abi.abi;
